99aaf9170e903da44aec961287050e786a00257b
minh vuong <bob.minhvuong@gmail.com>
2020-02-16 23:18:27 +0700

    fix

--- app-client/src/app/container/warehouse/import-warehouse/import-warehouse.component.ts
+++ app-client/src/app/container/warehouse/import-warehouse/import-warehouse.component.ts
@@ -1,3 +1,6 @@
+import { ProductService } from './../../../service/product/product.service';
+import { NzModalService, NzMessageService } from 'ng-zorro-antd';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportWarehouseComponent implements OnInit {
 
-  constructor() { }
+  isVisible = false;
+  pageIndex = 1;
+  pageSize = 50;
+  total = 1;
+  listOfData = [];
+  loading = true;
+  dataEdit: any | null = null;
+  filterForm: FormGroup;
+  constructor(
+    private modalService: NzModalService,
+    private fb: FormBuilder,
+    private message: NzMessageService,
+    private productSV: ProductService) { }
 
   ngOnInit() {
+    this.getAll();
+  }
+
+  getAll() {
+    let filter = {
+      find: '',
+      supllier_id : 0,
+      group_id :0,
+      active: true,
+      offset: 0,
+      limit: 20
+    };
+    this.productSV.getAllProduct(filter).subscribe(res => {
+      this.listOfData = res.data;
+      this.loading = false;
+      this.total = res.count;
+    });
+  }
+
+  handleCorU(client) {
+    this.dataEdit = client ? client : {};
+    this.isVisible = true;
+  }
+
+  closeModal(e) {
+    this.isVisible = e;
+    this.getAll();
+  }
+
+  filterData() {
+    this.getAll();
+  }
+
+  confirmDelete(id) {
+    this.modalService.confirm({
+      nzTitle: 'Bạn có chắc xóa trường này?',
+      nzOkText: 'Xác nhận',
+      nzOkType: 'danger',
+      nzOnOk: () => this.delete({ id: id }),
+      nzCancelText: 'Hủy',
+    });
+  }
+
+  delete(objId) {
+    this.productSV.deleteProduct(objId).subscribe(r => {
+      if (r && r.status == 1) {
+        this.message.create('success', 'Xóa thành công!');
+        this.getAll();
+      } else {
+        this.message.create('error', r && r.message ? r.message : 'Có lỗi xẩy ra. Vui lòng thử lại!');
+      }
+    })
   }
 
 }
+
+
